6e9957a3fd1aa021a3bc66e1c02900ab611fb485
aru <ag@empatica.com>
2015-06-04 14:38:22 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,27 @@
+# EmpaLink Sample Project
+
+## Introduction
+
+This sample project gives you the boilerplate code you need to connect to an Empatica E4 (or E3) device and start streaming data.
+
+The sample application implemented in the project has very simple functionalities:
+
+- It initializes the EmpaLink library with your API key.
+- If the previous step is successful, it starts scanning for Empatica devices, till it finds one that can be used with the API key you inserted in the code.
+- When such a device has been found, the app connects to the devices and streams data for 10 seconds, then it disconnects.
+
+## Setup
+
+- Clone / download this repository.
+- Open the sample project in Android Studio.
+- Make sure you have a valid API key. You can request one for your Empatica Connect account from our [Developer Area][1].
+- Edit `MainActivity.java` and assign your API key to the `EMPATICA_API_KEY` constant .
+- Download the Android SDK from our [Developer Area][1].
+- Unzip the archive you've downloaded and copy the `.aar` file you'll find inside into the `empalink-2.0` folder contained in the sample project.
+- Build and run the project.
+- If a device is in range and its light is blinking green, but the app doesn't connect, please check that the discovered device can be used with your API key. If the `allowed` parameter is always false, the device is not linked to your API key. Please check your [Developer Area][1].
+
+If you need any additional information about the Empatica API for Android, please check the [official documentation][1].
+
+[1]: https://www.empatica.com/connect/developer.php
+[2]: https://www.empatica.com/docs/android.php
---
+++ app/src/main/java/com/empatica/sample/MainActivity.java
@@ -0,0 +1,195 @@
+package com.empatica.sample;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.empatica.empalink.ConnectionNotAllowedException;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.config.EmpaSensorStatus;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+
+
+public class MainActivity extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long STREAMING_TIME = 10000; // Stops streaming 10 seconds after connection
+
+    private static final String EMPATICA_API_KEY = ""; // TODO insert your API Key here
+
+    private EmpaDeviceManager deviceManager;
+
+    private TextView accel_xLabel;
+    private TextView accel_yLabel;
+    private TextView accel_zLabel;
+    private TextView bvpLabel;
+    private TextView edaLabel;
+    private TextView ibiLabel;
+    private TextView temperatureLabel;
+    private TextView batteryLabel;
+    private TextView statusLabel;
+    private TextView deviceNameLabel;
+    private RelativeLayout dataCnt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize vars that reference UI components
+        statusLabel = (TextView) findViewById(R.id.status);
+        dataCnt = (RelativeLayout) findViewById(R.id.dataArea);
+        accel_xLabel = (TextView) findViewById(R.id.accel_x);
+        accel_yLabel = (TextView) findViewById(R.id.accel_y);
+        accel_zLabel = (TextView) findViewById(R.id.accel_z);
+        bvpLabel = (TextView) findViewById(R.id.bvp);
+        edaLabel = (TextView) findViewById(R.id.eda);
+        ibiLabel = (TextView) findViewById(R.id.ibi);
+        temperatureLabel = (TextView) findViewById(R.id.temperature);
+        batteryLabel = (TextView) findViewById(R.id.battery);
+        deviceNameLabel = (TextView) findViewById(R.id.deviceName);
+
+        // Create a new EmpaDeviceManager. MainActivity is both its data and status delegate.
+        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
+        // Initialize the Device Manager using your API key. You need to have Internet access at this point.
+        deviceManager.authenticateWithAPIKey(EMPATICA_API_KEY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        deviceManager.stopScanning();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        deviceManager.cleanUp();
+    }
+
+    @Override
+    public void didDiscoverDevice(BluetoothDevice bluetoothDevice, String deviceName, int rssi, boolean allowed) {
+        // Check if the discovered device can be used with your API key. If allowed is always false,
+        // the device is not linked with your API key. Please check your developer area at
+        // https://www.empatica.com/connect/developer.php
+        if (allowed) {
+            // Stop scanning. The first allowed device will do.
+            deviceManager.stopScanning();
+            try {
+                // Connect to the device
+                deviceManager.connectDevice(bluetoothDevice);
+                updateLabel(deviceNameLabel, "To: " + deviceName);
+            } catch (ConnectionNotAllowedException e) {
+                // This should happen only if you try to connect when allowed == false.
+                Toast.makeText(MainActivity.this, "Sorry, you can't connect to this device", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void didRequestEnableBluetooth() {
+        // Request the user to enable Bluetooth
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // The user chose not to enable Bluetooth
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            // You should deal with this
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void didUpdateSensorStatus(EmpaSensorStatus status, EmpaSensorType type) {
+        // No need to implement this right now
+    }
+
+    @Override
+    public void didUpdateStatus(EmpaStatus status) {
+        // Update the UI
+        updateLabel(statusLabel, status.name());
+
+        // The device manager is ready for use
+        if (status == EmpaStatus.READY) {
+            updateLabel(statusLabel, status.name() + " - Turn on your device");
+            // Start scanning
+            deviceManager.startScanning();
+        // The device manager has established a connection
+        } else if (status == EmpaStatus.CONNECTED) {
+            // Stop streaming after STREAMING_TIME
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    dataCnt.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Disconnect device
+                            deviceManager.disconnect();
+                        }
+                    }, STREAMING_TIME);
+                }
+            });
+        // The device manager disconnected from a device
+        } else if (status == EmpaStatus.DISCONNECTED) {
+            updateLabel(deviceNameLabel, "");
+        }
+    }
+
+    @Override
+    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+        updateLabel(accel_xLabel, "" + x);
+        updateLabel(accel_yLabel, "" + y);
+        updateLabel(accel_zLabel, "" + z);
+    }
+
+    @Override
+    public void didReceiveBVP(float bvp, double timestamp) {
+        updateLabel(bvpLabel, "" + bvp);
+    }
+
+    @Override
+    public void didReceiveBatteryLevel(float battery, double timestamp) {
+        updateLabel(batteryLabel, String.format("%.0f %%", battery * 100));
+    }
+
+    @Override
+    public void didReceiveGSR(float gsr, double timestamp) {
+        updateLabel(edaLabel, "" + gsr);
+    }
+
+    @Override
+    public void didReceiveIBI(float ibi, double timestamp) {
+        updateLabel(ibiLabel, "" + ibi);
+    }
+
+    @Override
+    public void didReceiveTemperature(float temp, double timestamp) {
+        updateLabel(temperatureLabel, "" + temp);
+    }
+
+    // Update a label with some text, making sure this is run in the UI thread
+    private void updateLabel(final TextView label, final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                label.setText(text);
+            }
+        });
+    }
+}
